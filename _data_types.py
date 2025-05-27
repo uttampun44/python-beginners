6b020e2960059f4fba74f47e964355eeca83ad2d
uttam pun <uttampun50@gmail.com>
2025-05-27 21:11:29 +0545

    data types

---
+++ _data_types.py
@@ -0,0 +1,26 @@
+#strings
+name = "uttam"
+
+print(name)
+
+#numbers
+num = 10
+
+print(num)
+
+#float
+price = 10.5
+
+print(price)
+
+#boolean
+isTrue = True
+isFalse = False
+
+print(isTrue)
+print(isFalse)  
+
+#none
+nothing = None
+print(nothing)
+
