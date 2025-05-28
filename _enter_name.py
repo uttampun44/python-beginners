47ab44ea123629614a9ccba8881a8b0a37e649f6
uttam pun <uttampun50@gmail.com>
2025-05-28 21:51:32 +0545

    strings

---
+++ _enter_name.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+
+number = input("Enter your number: ")
+print("Your name is:", name)
+print("Your number is:", int(number) + 10)
--- strings.py
+++ strings.py
@@ -5,3 +5,7 @@ print(shortName)
 
 print(len(name))
 print(name.endswith("pun"))
+print(name.capitalize())
+print(name.upper())
+print(name.count("t"))
+print(name.find("t"))
