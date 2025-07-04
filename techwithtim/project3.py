a795454b334ed6e1fb39fa307dffee008d218b7c
uttam pun <uttampun50@gmail.com>
2025-07-04 19:43:38 +0545

    project3 done

--- techwithtim/project3.py
+++ techwithtim/project3.py
@@ -3,13 +3,18 @@ fruits = ['apple', 'banana', 'orange', 'mango', 'grape', 'strawberry']
 for i in range(len(fruits)):
     print(fruits[i])
 
-person_Details = [];
+# list
+person_details = []
 person_name = input("Enter your name: ")
-person_age = input("Enter your age: ")
+person_age = int(input("Enter your age: "))
 person_email = input("Enter your email: ")
 
-person_Details.append(person_name)
-person_Details.append(person_age)
-person_Details.append(person_email)
+# dictionary
+person_Details = {
+    'name': person_name,
+    'age': person_age,
+    'email': person_email
+}
+person_details.append(person_Details)
 
 print(person_Details)
