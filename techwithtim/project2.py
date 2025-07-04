c7d787361ccb23c7194c76a15684475521707056
uttam pun <uttampun50@gmail.com>
2025-07-04 13:34:38 +0545

    project2 and project3 done

--- techwithtim/project2.py
+++ techwithtim/project2.py
@@ -1,20 +1,25 @@
-a = int(input("Enter a number One: "))
+def calculator():
+    try:
+        print("Welcome to the calculator!")
+        a = int(input("Enter a number One: "))
 
-operator = input("Enter an operator: ")
+        operator = input("Enter an operator: ")
 
-b = int(input("Enter a number Two: "))
+        b = int(input("Enter a number Two: "))
 
 #switch case
-match operator:
-    case "+":
-      print(a + b)
-    case "-":
-      print(a - b)
-    case "*":
-      print(a * b)
-    case "/":
-     print(a / b)
-    case _:
-      print(f"Invalid operator {operator}")
-   
+        match operator:
+          case "+":
+           print("The result is: ", a + b)
+          case "-":
+           print("The result is: ", a - b)
+          case "*":
+           print("The result is: ", a * b)
+          case "/":
+           print("The result is: ", a / b)
+          case _:
+            print(f"Invalid operator {operator}")
+    except ValueError:
+      print("Invalid input. Please enter numbers only.")
+calculator()   
     
---
+++ techwithtim/project3.py
@@ -0,0 +1,15 @@
+fruits = ['apple', 'banana', 'orange', 'mango', 'grape', 'strawberry']
+
+for i in range(len(fruits)):
+    print(fruits[i])
+
+person_Details = [];
+person_name = input("Enter your name: ")
+person_age = input("Enter your age: ")
+person_email = input("Enter your email: ")
+
+person_Details.append(person_name)
+person_Details.append(person_age)
+person_Details.append(person_email)
+
+print(person_Details)
